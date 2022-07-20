0d2c4ff05c1fd89b798a66643d006b9e37902c06
Bishnu <bishnuthapako@gmail.com>
2022-07-19 20:16:24 -0400

    completed

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+    
+    def initialize(word) 
+        @word=word   
+    end
+
+    def match(string)
+        string.select do |str|
+            str.split("").sort ==word.split("").sort
+        end
+    end
+  
+  
+end
+# f1=Anagram.new('Listen')
